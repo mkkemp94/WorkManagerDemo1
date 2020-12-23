de9b9a27b54f2915788101e2400e9ac57a596bc9
MKemp <mkemp@accu-time.com>
2020-12-23 11:18:55 -0500

    Create and execute a one time work request on button press

--- app/src/main/java/com/mkemp/workmanagerdemo1/MainActivity.kt
+++ app/src/main/java/com/mkemp/workmanagerdemo1/MainActivity.kt
@@ -2,6 +2,9 @@ package com.mkemp.workmanagerdemo1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity()
 {
@@ -9,5 +12,16 @@ class MainActivity : AppCompatActivity()
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button.setOnClickListener {
+            setOneTimeWorkRequest()
+        }
+    }
+    
+    private fun setOneTimeWorkRequest() {
+        
+        val uploadRequest = OneTimeWorkRequest.Builder(UploadWorker::class.java).build()
+        
+        WorkManager.getInstance(applicationContext).enqueue(uploadRequest)
+        
     }
 }
---
+++ app/src/main/java/com/mkemp/workmanagerdemo1/UploadWorker.kt
@@ -0,0 +1,24 @@
+package com.mkemp.workmanagerdemo1
+
+import android.content.Context
+import android.util.Log
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import java.lang.Exception
+
+class UploadWorker(context: Context, params: WorkerParameters) : Worker(context, params) {
+
+    override fun doWork(): Result {
+
+        try {
+            for (i in 0..600) {
+                Log.i("MyTag", "Uploading $i")
+            }
+            return Result.success()
+        } catch (e: Exception) {
+            return Result.failure()
+        }
+
+    }
+
+}
