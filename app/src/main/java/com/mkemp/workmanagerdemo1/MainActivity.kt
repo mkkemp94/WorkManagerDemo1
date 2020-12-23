e9db5f4010bf2c77e9e6fe542dbde4e72b996f59
MKemp <mkemp@accu-time.com>
2020-12-23 14:05:14 -0500

    Set constraints for work

--- app/src/main/java/com/mkemp/workmanagerdemo1/MainActivity.kt
+++ app/src/main/java/com/mkemp/workmanagerdemo1/MainActivity.kt
@@ -3,6 +3,8 @@ package com.mkemp.workmanagerdemo1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.Observer
+import androidx.work.Constraints
+import androidx.work.NetworkType
 import androidx.work.OneTimeWorkRequest
 import androidx.work.WorkManager
 import kotlinx.android.synthetic.main.activity_main.*
@@ -21,7 +23,15 @@ class MainActivity : AppCompatActivity()
     private fun setOneTimeWorkRequest() {
         
         val workManager = WorkManager.getInstance(applicationContext)
-        val uploadRequest = OneTimeWorkRequest.Builder(UploadWorker::class.java).build()
+
+        val constraints = Constraints.Builder()
+            .setRequiresCharging(true)
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+
+        val uploadRequest = OneTimeWorkRequest.Builder(UploadWorker::class.java)
+            .setConstraints(constraints)
+            .build()
         
         workManager.enqueue(uploadRequest)
         workManager.getWorkInfoByIdLiveData(uploadRequest.id)
